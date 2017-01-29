406e1ed4d25fe80e37eb6489927c103eac5c0d6f
dvt <danevantol51247@gmail.com>
2017-01-28 20:50:50 -0700

    Create CommonProxy.java

---
+++ dvt/ttm/CommonProxy.java
@@ -0,0 +1,22 @@
+package dvt.ttm;
+
+import dvt.ttm.event.EventHandlerCommon;
+import dvt.ttm.items.ModItems;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class CommonProxy {
+	public void preInit(FMLPreInitializationEvent e) {
+		ModItems.createItems();
+	}
+	
+	public void init(FMLInitializationEvent e) {
+		MinecraftForge.EVENT_BUS.register(new EventHandlerCommon());
+	}
+	
+	public void postInit(FMLPostInitializationEvent e) {
+		
+	}
+}
