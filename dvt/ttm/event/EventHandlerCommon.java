be6d8017c6aa1407c2268e6ec1d47e76bc530a43
dvt <danevantol51247@gmail.com>
2017-01-29 18:04:25 -0700

    Update EventHandlerCommon.java

--- dvt/ttm/event/EventHandlerCommon.java
+++ dvt/ttm/event/EventHandlerCommon.java
@@ -2,15 +2,23 @@ package dvt.ttm.event;
 
 import dvt.ttm.items.ModItems;
 import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EventHandlerCommon {
+	public BlockPos receiverPos;
+	
 	@SubscribeEvent
 	public void onHarvestDrops(HarvestDropsEvent e) {
-		Item inUse = e.getHarvester().getActiveItemStack().getItem();
-		if (inUse == ModItems.TeleportAxe || inUse == ModItems.TeleportHoe || inUse == ModItems.TeleportPickaxe || inUse == ModItems.TeleportShovel) {
-			
+		if (e != null && e.getHarvester() != null) {
+			Item inUse = e.getHarvester().getHeldItemMainhand().getItem();
+			for (int i = 0; i < ModItems.TeleportToolsList.length; i++) {
+				if (inUse == ModItems.TeleportToolsList[i]) {
+					e.setDropChance(0.0F);
+					break;
+				}
+			}
 		}
 	}
 }
