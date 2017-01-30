ec6e6a98bcc6db12a560fdb8b035c8ea09670f30
dvt <danevantol51247@gmail.com>
2017-01-29 18:01:07 -0700

    Update ClientProxy.java

--- dvt/ttm/ClientProxy.java
+++ dvt/ttm/ClientProxy.java
@@ -1,5 +1,15 @@
 package dvt.ttm;
 
+import dvt.ttm.blocks.BlockRenderRegister;
+import dvt.ttm.items.ItemRenderRegister;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
 public class ClientProxy extends CommonProxy {
-	
+	@Override
+	public void init(FMLInitializationEvent e) {
+		super.init(e);
+		
+		ItemRenderRegister.registerItemRenderer();
+		BlockRenderRegister.registerBlockRenderer();
+	}
 }
