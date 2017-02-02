ba2d5ce6c56bb5c47d14c02166d9a0154dd20c51
dvt <danevantol51247@gmail.com>
2017-02-01 19:22:29 -0700

    Update EventHandlerCommon.java

--- dvt/ttm/event/EventHandlerCommon.java
+++ dvt/ttm/event/EventHandlerCommon.java
@@ -1,14 +1,14 @@
 package dvt.ttm.event;
 
 import dvt.ttm.items.ModItems;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EventHandlerCommon {
-	public BlockPos receiverPos;
-	
 	@SubscribeEvent
 	public void onHarvestDrops(HarvestDropsEvent e) {
 		if (e != null && e.getHarvester() != null) {
@@ -17,6 +17,27 @@ public class EventHandlerCommon {
 				if (inUse == ModItems.TeleportToolsList[i]) {
 					e.setDropChance(0.0F);
 					break;
+				}
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onPlayerInteract(RightClickBlock e) {
+		if (e != null) {
+			Item inUse = e.getItemStack().getItem();
+			if (e.getWorld().getTileEntity(e.getPos()) instanceof IInventory) {
+				for (int i = 0; i < ModItems.TeleportToolsList.length; i++) {
+					if (inUse == ModItems.TeleportToolsList[i]) {
+						int[] loc = {e.getPos().getX(), e.getPos().getY(), e.getPos().getZ()};
+						NBTTagCompound compound = e.getItemStack().getTagCompound();
+						if (compound != null) {
+							compound = new NBTTagCompound();
+						}
+						System.out.println(compound);
+						compound.setIntArray("teleportPos", loc);
+						e.getItemStack().setTagCompound(compound);
+					}
 				}
 			}
 		}
